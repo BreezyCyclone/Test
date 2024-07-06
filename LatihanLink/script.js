241c648145b848049fbe65f96f63b8c2d9c34339
ZenMuhamadX <muhamadzen669@gmail.com>
2024-07-06 09:37:06 +0700

    styling latihan1

--- LatihanLink/script.js
+++ LatihanLink/script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("closeTab");
-        button.addEventListener("click", (e) => {
-            e.preventDefault();
-            window.close();
-        });
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.close();
+});
